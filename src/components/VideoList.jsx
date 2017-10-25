c223bf2a4f63e29c8f6d300788f920d1c7c06054
aikerim <aygerim.sauletkhan@gmail.com>
2017-10-25 15:39:44 -0700

    started first part

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,16 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+var VideoList = (props) => {
+
+  return (
+    <div className="video-list">
+      {props.exampleVideoData.map(video => 
+        
+          <VideoListEntry title = {video.snippet.title} 
+          description = {video.snippet.description} src = {video.snippet.thumbnails.default.url} />
+          
+          )} 
+    </div>
+  );
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -16,4 +20,10 @@ VideoList.propTypes = {
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
-window.VideoList = VideoList;
+window.VideoList = VideoList(window.exampleVideoData);
+
+// <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,11 +1,11 @@
-var VideoListEntry = () => (
+var VideoListEntry = (props) => (
   <div className="video-list-entry media">
     <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
+      <img className="media-object" src={props.src} alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
+      <div className="video-list-entry-title">{props.title}</div>
+      <div className="video-list-entry-detail">{props.description}</div>
     </div>
   </div>
 );
@@ -18,4 +18,4 @@ VideoListEntry.propTypes = {
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-window.VideoListEntry = VideoListEntry;
+window.VideoListEntry = VideoListEntry();
